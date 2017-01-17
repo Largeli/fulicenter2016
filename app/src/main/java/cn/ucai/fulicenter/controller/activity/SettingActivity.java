20ab963d5def100cfbae308cc6d93b2816e999e5
lijianjun <1020942021@qq.com>
2017-01-17 19:38:16 +0800

    个人中心数据的显示

---
+++ app/src/main/java/cn/ucai/fulicenter/controller/activity/SettingActivity.java
@@ -0,0 +1,15 @@
+package cn.ucai.fulicenter.controller.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import cn.ucai.fulicenter.R;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
--- app/src/main/java/cn/ucai/fulicenter/controller/fragment/PersonalCenterFragment.java
+++ app/src/main/java/cn/ucai/fulicenter/controller/fragment/PersonalCenterFragment.java
@@ -6,14 +6,29 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.controller.application.FuLiCenterApplication;
+import cn.ucai.fulicenter.model.bean.User;
+import cn.ucai.fulicenter.model.utils.ImageLoader;
+import cn.ucai.fulicenter.view.MFGT;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PersonalCenterFragment extends Fragment {
 
+
+    @BindView(R.id.iv_user_Avatar)
+    ImageView mIvUserAvatar;
+    @BindView(R.id.tv_user_name)
+    TextView mTvUserName;
+    Unbinder unbinder;
 
     public PersonalCenterFragment() {
         // Required empty public constructor
@@ -23,8 +38,29 @@ public class PersonalCenterFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_personal_center, container, false);
+        View layout = inflater.inflate(R.layout.fragment_personal_center, container, false);
+        unbinder = ButterKnife.bind(this, layout);
+        inintData();
+        return layout;
     }
 
+    private void inintData() {
+        User user = FuLiCenterApplication.getUser();
+        if(user!=null){
+            loadUserInfo(user);
+        }else {
+            MFGT.gotoLogin(getActivity());
+        }
+    }
+
+    private void loadUserInfo(User user) {
+        ImageLoader.downloadImg(getContext(),mIvUserAvatar,user.getAvatarPath() );
+        mTvUserName.setText(user.getMuserNick());
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 }
