692b358b1ab98d7b323c750111ec56833fe98fb3
lijianjun <1020942021@qq.com>
2017-01-19 16:22:56 +0800

    收藏的Adapter的编写

---
+++ app/src/main/java/cn/ucai/fulicenter/controller/adapter/CollectAdapter.java
@@ -0,0 +1,149 @@
+package cn.ucai.fulicenter.controller.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.controller.activity.GoodsDetailsActivity;
+import cn.ucai.fulicenter.controller.application.I;
+import cn.ucai.fulicenter.model.bean.CollectBean;
+import cn.ucai.fulicenter.model.net.IModelNewGoods;
+
+/**
+ * Created by Administrator on 2017/1/10 0010.
+ */
+
+public class CollectAdapter extends RecyclerView.Adapter {
+    static final int TYPE_ITEM = 0;
+    static final int TYPE_FOOTER = 1;
+    ArrayList<CollectBean> arrayList;
+    Context context;
+    String footer;
+    boolean isMore = true;
+    boolean isDragging;
+    IModelNewGoods model;
+
+    public CollectAdapter(ArrayList<CollectBean> arrayList, Context context, IModelNewGoods model) {
+        this.arrayList = arrayList;
+        this.context = context;
+        this.model = model;
+    }
+
+    public void initData(ArrayList<CollectBean> arrayList) {
+        this.arrayList.clear();
+        addData(arrayList);
+    }
+
+    public void addData(ArrayList<CollectBean> arrayList) {
+        this.arrayList.addAll(arrayList);
+        notifyDataSetChanged();
+    }
+
+    public String getFooter() {
+        return footer;
+    }
+
+    public void setFooter(String footer) {
+        this.footer = footer;
+        notifyDataSetChanged();
+    }
+
+    public boolean isMore() {
+        return isMore;
+    }
+
+    public void setMore(boolean more) {
+        isMore = more;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View layout;
+        RecyclerView.ViewHolder holder = null;
+        switch (viewType) {
+            case TYPE_FOOTER:
+                layout = layoutInflater.inflate(R.layout.item_newgoods_footer, null);
+                holder = new FooterViewHolder(layout);
+                break;
+            case TYPE_ITEM:
+                layout = layoutInflater.inflate(R.layout.item_collect, null);
+                holder = new CollectViewHolder(layout);
+                break;
+        }
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder parentholder, final int position) {
+        if (position == getItemCount() - 1) {
+            FooterViewHolder holder = (FooterViewHolder) parentholder;
+            holder.tvfooter.setText(isMore ? "上拉加载更多" : "没有更多数据");
+            return;
+        }
+        CollectViewHolder holder = (CollectViewHolder) parentholder;
+        final CollectBean contact = arrayList.get(position);
+        holder.tvGoosName.setText(contact.getGoodsName());
+        model.downloadnewgoodsImage(context, contact.getGoodsImg(), R.drawable.nopic, holder.ivGoodsThumb);
+        //Log.e("main",)
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, GoodsDetailsActivity.class);
+                intent.putExtra(I.GoodsDetails.KEY_GOODS_ID, arrayList.get(position).getGoodsId());
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size() + 1;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == getItemCount() - 1) {
+            return TYPE_FOOTER;
+        }
+        return TYPE_ITEM;
+    }
+
+    class FooterViewHolder extends RecyclerView.ViewHolder {
+        TextView tvfooter;
+
+        public FooterViewHolder(View itemView) {
+            super(itemView);
+            tvfooter = (TextView) itemView.findViewById(R.id.tv_newgoods_footer);
+        }
+    }
+
+
+    static class CollectViewHolder extends RecyclerView.ViewHolder{
+        @BindView(R.id.ivGoodsThumb)
+        ImageView ivGoodsThumb;
+        @BindView(R.id.tvGoosName)
+        TextView tvGoosName;
+        @BindView(R.id.iv_collect_del)
+        ImageView ivCollectDel;
+        @BindView(R.id.layout_goods)
+        RelativeLayout layoutGoods;
+
+        CollectViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+}
